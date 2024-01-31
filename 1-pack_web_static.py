79b29bf36412f58effc98669e898fb78e668d5f3
maramilod <marammilod03@gmail.com>
2024-01-31 22:01:45 +0000

    add

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# Fabfile to generates a .tgz archive from the contents
+from datetime import datetime
+import os.path
+from fabric.api import local
+
+
+def do_pack():
+    """Create a tar archive"""
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
