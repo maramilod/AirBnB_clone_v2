86c511376658b6cb9caf9c9326d630f41acf5eed
maramilod <marammilod03@gmail.com>
2024-01-31 21:40:05 +0000

    add

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -25,6 +25,10 @@ printf %s "server {
 	index index.html index.htm;
     }
 
+        location /redirect_me {
+	return 301 http://cuberule.com/;
+    }
+
     error_page 404 /404.html;
     location /404 {
       root /var/www/html;
