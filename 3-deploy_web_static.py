cb708181d673d0bb1589be939997958d8fc2496a
maramilod <marammilod03@gmail.com>
2024-01-31 22:22:51 +0000

    add

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
+# Fabfile to create an archive
 import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+from datetime import datetime
+from fabric.api import env, local, put, run
 
-env.hosts = ["52.201.220.108", "35.175.132.106"]
+env.hosts = ["54.160.85.72", "52.201.220.108"]
+
+
+def do_pack():
+    """Create a tar gzipped archive of the directory web_static."""
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
 
 
 def do_deploy(archive_path):
@@ -46,3 +62,11 @@ def do_deploy(archive_path):
            format(name)).failed is True:
         return False
     return True
+
+
+def deploy():
+    """Create and distribute an archive to a web server."""
+    file = do_pack()
+    if file is None:
+        return False
+    return do_deploy(file)
